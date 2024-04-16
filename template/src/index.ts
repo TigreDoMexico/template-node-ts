c0ad118da378738b2c0d1cd443cc389e94cd29a2
David Tigre Moraes <tigredavid007@gmail.com>
2024-04-16 20:00:02 -0300

    alterar numero 3 para 4

--- template/src/index.ts
+++ template/src/index.ts
@@ -2,5 +2,5 @@ import { soma, subtracao } from "./module/calculator"
 
 console.log("Hello ${{ values.name }}")
 
-console.log(soma(3, 3))
-console.log(subtracao(3, 3))
+console.log(soma(4, 4))
+console.log(subtracao(4, 3))
